fd29447703f65299216fdf14386b3475fe4f5d51
Christopher Babb <chris@appsoftdev.com>
2022-06-08 09:05:30 -0700

    commit tree 388f428a5ac3780a51c3ad7290c7ad8989dfe7dc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driversList) {
+    let newDriversList = [driversList[0], driversList[1]]
+    return newDriversList
+}
+const returnLastTwoDrivers = function (driversList) {
+    let newDriversList = [driversList[driversList.length - 2], driversList[driversList.length -1]]
+    return newDriversList
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(inputValue) {
+    return function (fareMult) { return fareMult * inputValue }
+}
+
+function fareDoubler(fareToDouble) {
+    return fareToDouble * 2
+}
+function fareTripler(fareToTriple) {
+    return fareToTriple * 3
+}
+function selectDifferentDrivers(arrayOfDrivers, passedFunction) {
+    return passedFunction(arrayOfDrivers)
+}
+let foo = function () {
+    return "bar";
+  };
